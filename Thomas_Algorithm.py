85f62027e074878697c6d6b2215381d3937bc227
tului <tgantulga@ucdavis.edu>
2020-12-25 12:25:40 -0800

    updated

--- Thomas_Algorithm.py
+++ Thomas_Algorithm.py
@@ -17,10 +17,8 @@ def create_matrix(n, a=1, b=1, c=1):
 
 
 matrix = create_matrix(5, 5, 8, 6)
-
-
-
-# print(matrix)
+print(np.round(matrix, 3))
+print
 
 
 def first(mat):
@@ -30,21 +28,47 @@ def first(mat):
     return mat
 
 first(matrix)
+print(np.round(matrix, 1))
+print
 
-def between(mat):
-    # for i in range(1, 4):
-    i = 1
-    ai = mat[i][i - 1]
 
-    for j in [-1, 0, 1]:
-        mat[i][i + j] /= ai
-        # mat[i][i + j] -= mat[i - 1][i + j]
+def between1(mat):
+    for i in range(1, 4):
+        ai = mat[i][i - 1]
 
+        for j in [-1, 0, 1]:
+            mat[i][i + j] /= ai
+            mat[i][i + j] -= mat[i - 1][i + j] / mat[i - 1][i - 1]
     return mat
 
 
-pprint(between(matrix))
-# for i in range(1, 3):
-#     print(i)
+between1(matrix)
+print(np.round(matrix, 1))
+print
 
+def last(mat):
+    an = mat[-1][-2]
+    mat[-1][-1] /= an
+    mat[-1][-2] /= an
+
+    for i in [-2, -1]:
+        mat[-1][i] -= mat[-2][i] / mat[-2][-2]
+
+    mat[-1][-1] /= mat[-1][-1]
+    return mat
+
+last(matrix)
+print(np.round(matrix, 1))
+print
+
+def between2(mat):
+
+    iter = -1 * np.arange(len(mat) - 1) - 2
+    for i in iter:
+        print(mat[i][i])
+
+    print
+    return mat
+
+print(between2(matrix))
 
